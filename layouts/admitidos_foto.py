5d95d0ce851fdb4191eb4cb222e9bc3f7035269e
Cristrian <cctrianag@unal.edu.co>
2021-10-15 23:44:09 -0500

    Update: data uploaded

---
+++ layouts/admitidos_foto.py
@@ -0,0 +1,6 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def generate_layout(appname):
+    layout = []
+    return layout
---
+++ layouts/admitidos_live.py
@@ -0,0 +1,8 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def generate_layout(appname):
+
+    dropdowns = []
+    layout = html.Div([])
+    return layout
