e169f05d1faf88bdc2b4241951565041ef8aa020
Cristrian <cctrianag@unal.edu.co>
2021-10-17 17:45:01 -0500

    Update: callbacks Update

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from dash import Dash, dcc, html
 from dash.html.H1 import H1
+from pandas.io.sql import has_table
 
 import callbacks
+from data import data
 from layouts import admitidos_live, admitidos_foto
 app = Dash(__name__)
 
 #Import Data
-
+df = data.get_dataframe()
 
 live_layout = admitidos_live.generate_layout('live')
 foto_layout = admitidos_foto.generate_layout('foto')
+
 
 layout = html.Div([
     html.H1('Tablero Admitidos'),
@@ -19,10 +22,12 @@ layout = html.Div([
         dcc.Tab(label='Fotografía', value='tab-foto'),
         ], 
         value='tab-en-vivo'),
-    html.Div(id='page-content', children=live_layout)
+    html.Div(id='page-content', children=live_layout),
+    html.Div(id='test-out')
 ])
 app.layout = layout
 
+test = callbacks.generate_graphics_callbacks(app, 'live', df)
 
 if __name__ == '__main__':
     
--- callbacks.py
+++ callbacks.py
@@ -4,16 +4,14 @@ import pandas as pd
 
 def generate_graphics_callbacks(app: Dash, appname: str, df: pd.DataFrame):
     
-    app.callback(
-        Output(f'{appname}-admitidos-bar'),
-        Output(f'{appname}-modalidad-pie'),
-        Output(f'{appname}-metodologia-pie'),
-        [Input(f'{appname}-periodo-dropdown'),
-        Input(f'{appname}-territorial-dropdown'),
-        Input(f'{appname}-cetap-dropdown'),
-        Input(f'{appname}-modalidad-dropdown'),
-        Input(f'{appname}-metodologia-dropdown'),
-        Input(f'{appname}-programa-dropdown')],
+    @app.callback(
+        Output('test-out', 'children'),
+        [Input(f'{appname}-periodo-dropdown', 'value'),
+        Input(f'{appname}-territorial-dropdown', 'value'),
+        Input(f'{appname}-cetap-dropdown', 'value'),
+        Input(f'{appname}-modalidad-dropdown', 'value'),
+        Input(f'{appname}-metodologia-dropdown', 'value'),
+        Input(f'{appname}-programa-dropdown', 'value')],
     )
     def generate_graphs(*values):
         return values
